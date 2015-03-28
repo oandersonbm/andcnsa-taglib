3eb418992190af916e323aa2d28cad421fe5e1ce
andcnsa <andcnsamobile@gmail.com>
2015-03-28 00:42:37 -0300

    Criação de um esboço para DataList
    Início de um dos componentes mais indispensáveis na criação do sistema;
    a lista.
    Utilizando UIData, mesmo componente utilizado pelo primefaces, mas
    preciso realizar uma análise sobre como funcionará.

---
+++ src/com/andcnsa/component/datalist/DataList.java
@@ -0,0 +1,10 @@
+package com.andcnsa.component.datalist;
+
+import javax.faces.component.UIData;
+
+public class DataList extends UIData{
+	@Override
+	public String getFamily() {
+		return "layout";
+	}
+}
