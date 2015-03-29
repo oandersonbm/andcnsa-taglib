79b1e1b46add091f924f88e26798ad729f4eabca
andcnsa <andcnsamobile@gmail.com>
2015-03-29 13:48:16 -0300

    Mudança no form no autocomplete

--- src/com/andcnsa/component/form/Form.java
+++ src/com/andcnsa/component/form/Form.java
@@ -8,12 +8,20 @@ import javax.faces.context.ResponseWriter;
 
 public class Form extends UIForm{
 
-	//private enum Propriedades{}
+	private enum Propriedades{
+		autocomplete
+	}
+	public String getAutocomplete(){
+		return (String)getStateHelper().eval(Propriedades.autocomplete, "off");
+	}
+	public void setAutocomplete(String autocomplete){
+		getStateHelper().put(Propriedades.autocomplete, autocomplete);
+	}
 	@Override
 	public void encodeBegin(FacesContext context) throws IOException{
 		ResponseWriter out = context.getResponseWriter();
 		String clientId = getClientId(context);
-		out.write("<form id='"+clientId+"' name='"+clientId+"' method='post' enctype='utf-8' autocomplete='off'>");	
+		out.write("<form id='"+clientId+"' name='"+clientId+"' method='post' enctype='utf-8' autocomplete='"+getAutocomplete()+"'>");	
 		out.write("<input type='hidden' id='"+clientId+"' name='"+clientId+"' value='"+clientId+"'>");		
 	}
 }
