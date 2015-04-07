ae73dc8fba0cd3e4e6de612396ae426f8de4ebd0
Anderson Magalhães <andcnsamobile@gmail.com>
2015-04-07 16:11:02 -0300

    Tentativa de criar um taglib nos moldes da anterior para o sistema do Lucas
    Gerei um arquivo ANT que propicia criar uma andcnsa-taglib para o
    sistema do Lucas, porém, ainda não realizei os testes necessários para
    verificar os impactos que a atualização acarretará ao sistema dele.

---
+++ build/andcnsa-taglib/META-INF/resources/andcnsa/js/cdigital.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('input.data').datepicker({
+	    format: "dd/mm/yyyy",
+	    language: "pt-BR",
+	    multidate: false,
+	    autoclose: true,
+	    todayHighlight: true
+	});
+});
