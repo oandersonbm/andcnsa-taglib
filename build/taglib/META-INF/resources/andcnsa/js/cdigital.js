d7098de53920d17cb631e26c5f7a4999314b6f9b
Anderson Magalhães <andcnsamobile@gmail.com>
2015-04-15 13:13:45 -0300

    Adicionei o TabContainer e o Tab.

--- build/taglib/META-INF/resources/andcnsa/js/cdigital.js
+++ build/taglib/META-INF/resources/andcnsa/js/cdigital.js
@@ -1,9 +1,6 @@
-$(function(){
-	$('input.data').datepicker({
-	    format: "dd/mm/yyyy",
-	    language: "pt-BR",
-	    multidate: false,
-	    autoclose: true,
-	    todayHighlight: true
+$(function() {
+	$('.nav-tabs a').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
 	});
 });
